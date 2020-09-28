831209a2138145c2b337f823392f9eabd1466ede
Pedro Henrique de Medeiros <55166993+pedrohenm@users.noreply.github.com>
2020-09-28 20:03:44 -0300

    Update Quadrado.java

--- Questão 02/Quadrado.java
+++ Questão 02/Quadrado.java
@@ -4,6 +4,19 @@ Implemente uma classe `Quadrado`, que possui um atributo
 o atributo `lado`. Faça com que essa classe __implemente__ a 
 interface `Geometria`.
 */
-public class Quadrado {
+public class Quadrado implements Geometria{
+  private double Lado;
+  
+  public Quadrado(double lado){
+    this.Lado = lado;
+  }
+  
+  @Override
+  public void getPerimetro() {
+  }
+
+  @Override
+  public void getArea() {
+  }
 
 }
